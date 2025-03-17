02f952e8176e16c003da987b03ae0682d3f12a51
Apfirebolt <aspper20@gmail.com>
2025-03-17 23:28:49 +0530

    added staff and student pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
-import HomePage from "./screens/Home.tsx";
-import Spells from "./screens/Spells.tsx";
+
+const HomePage = React.lazy(() => import("./screens/Home.tsx"));
+const Spells = React.lazy(() => import("./screens/Spells.tsx"));
+const Students = React.lazy(() => import("./screens/Students.tsx"));
+const Staff = React.lazy(() => import("./screens/Staff.tsx"));
 
 const App = () => {
   return (
@@ -13,6 +16,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage/>} />
           <Route path="/spells" element={<Spells/>} />
+          <Route path="/students" element={<Students/>} />
+          <Route path="/staff" element={<Staff/>} />
         </Routes>
       <Footer />
       <ToastContainer />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const HeaderComponent: React.FC = () => {
+
+  interface Link {
+    name: string;
+    url: string;
+  }
+
+  const links: Link[] = [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "Characters",
+      url: "/characters",
+    },
+    {
+      name: "Spells",
+      url: "/spells",
+    },
+    {
+      name: "Students",
+      url: "/students",
+    },
+    {
+      name: "Staff",
+      url: "/staff",
+    },
+  ];
   return (
     <nav className="bg-primary-100 border-gray-200 px-2 sm:px-4 py-4 dark:bg-gray-900">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -32,15 +60,16 @@ const HeaderComponent: React.FC = () => {
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/characters">Characters</Link>
-            </li>
-            <li>
-              <Link to="/spells">Spells</Link>
-            </li>
+            {links.map((link) => (
+              <li key={link.name}>
+                <Link
+                  to={link.url}
+                  className="block py-2 text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
@@ -49,3 +78,6 @@ const HeaderComponent: React.FC = () => {
 };
 
 export default HeaderComponent;
+
+
+
